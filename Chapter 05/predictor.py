68a47f39a4d71bc3e46ab66938416d55a16566d1
lys <kyym10041@naver.com>
2025-02-20 16:41:08 +0900

    Add Thompson sampling

--- Chapter 05/predictor.py
+++ Chapter 05/predictor.py
@@ -13,3 +13,29 @@ for i in range(N):
         # 임의의 (0, 1) 범위 내의 부동소수점 값이 해당 슬롯 머신의 전환율보다 작은지 확인
         if np.random.rand() < conversionRates[j]:   
             X[i][j] = 1 
+
+# 각 슬롯머신별 승패 수를 저장할 배열 생성
+nPosReward = np.zeros(d)
+nNegReward = np.zeros(d)
+
+# 톰슨 샘플링
+for i in range(N):
+    selected = 0    # 선택된 슬롯머신
+    maxRandom = 0   # 모든 슬롯머신에서 가장 높은 베타 분포 추정을 얻기 위해 사용됨
+
+    for j in range(d):
+        randomBeta = np.random.beta(nPosReward[j] + 1, nNegReward[j] + 1)
+        if randomBeta > maxRandom:
+            maxRandom = randomBeta
+            selected = j
+
+    if X[i][selected] == 1:
+        nPosReward[selected] += 1
+    else:
+        nNegReward[selected] += 1
+
+# 최고라고 생각되는 슬롯머신 표시
+nSelected = nPosReward + nNegReward
+for i in range(d):
+    print(f'Machine number: {i + 1} was selectd {nSelected[i]} times')
+print(f'Conclusion: Best machine is machine number {np.argmax(nSelected) + 1}')
